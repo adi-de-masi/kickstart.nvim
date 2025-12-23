0498508dd7caa0612cb26ca399a91b003e6ee693
Adi De Masi <adi.demasi@swissmarketplace.group>
2025-12-23 23:41:31 +0100

    add copilot to the mix using blink

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -2,24 +2,19 @@ return {
   'zbirenbaum/copilot.lua',
   cmd = 'Copilot',
   event = 'InsertEnter',
-  -- Load before nvim-cmp to ensure copilot is ready
-  priority = 100,
-  config = function()
-    require('copilot').setup {
-      suggestion = {
-        enabled = true,
-        auto_trigger = true, -- Need to trigger to get suggestions for copilot-cmp
-        debounce = 75,
-        keymap = {
-          accept = false, -- Disable accept keymap, use copilot-cmp instead
-          accept_word = false,
-          accept_line = false,
-          next = '<M-]>',
-          prev = '<M-[>',
-          dismiss = '<C-]>',
-        },
+  dependencies = {
+    'copilotlsp-nvim/copilot-lsp',
+  },
+  opts = {
+    nes = {
+      enabled = true,
+      keymap = {
+        accept_and_goto = '<leader>p',
+        accept = false,
+        dismiss = '<Esc>',
       },
-      panel = { enabled = true },
-    }
-  end,
+    },
+    suggestion = { enabled = false },
+    panel = { enabled = false },
+  },
 }
